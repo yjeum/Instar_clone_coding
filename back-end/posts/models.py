d04bb7986aa6be50c96627be9a136037ee4bd5ba
nas <nastorond@kakao.com>
2023-10-27 20:28:45 +0900

    [UPDATE] add pillow in requirements

--- back-end/posts/models.py
+++ back-end/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Post(models.Model):
+    uesr = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='like_posts'
+        )
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.BooleanField()
