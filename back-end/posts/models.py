144c5bea4eb01ee64557d197782434bc7072e738
nas <nastorond@kakao.com>
2023-10-27 21:40:47 +0900

    [UPDATE] add models

--- back-end/posts/models.py
+++ back-end/posts/models.py
@@ -8,9 +8,23 @@ class Post(models.Model):
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
         related_name='like_posts'
-        )
+    )
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    likes = models.BooleanField()
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='like_comments'
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
