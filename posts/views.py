9b65048f0ea84f9c90a1cbf09463f2a3732501bc
YeJinEum <yjeum03@gmail.com>
2023-11-03 21:07:23 +0900

    [CREATE] posts > views > index, detail

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from django.http import JsonResponse
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+    }
+    return render(request, "posts/index.html", context)
+
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {"post": post}
+    return JsonResponse(context)
