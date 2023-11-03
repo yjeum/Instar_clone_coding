9699c912b0ac5e980d1c8548821020b72f6977fc
YeJinEum <yjeum03@gmail.com>
2023-11-03 22:06:51 +0900

    [수정중]

--- posts/urls.py
+++ posts/urls.py
@@ -5,16 +5,16 @@ app_name = "posts"
 urlpatterns = [
     path("", views.index),
     path("<int:pk>/", views.detail),
-    path("create/", views.create, name="create"),
-    path("<int:pk>/update/", views.update, name="update"),
-    path("<int:pk>/delete/", views.delete, name="delete"),
-    path("<int:post_pk>/comment/create", views.comment_create),  # 댓글 작성
-    path(
-        "<int:post_pk>/comment/<int:comment_pk>/update", views.comment_update
-    ),  # 댓글 수정
-    path("<int:post_pk>/comment", views.comment),  # 댓글 보기
-    path(
-        "<int:post_pk>/comment/<int:comment_pk>/delete", views.comment_delete
-    ),  # 댓글 삭제
-    path("<int:post_pk>/commemet/<int:parent_id>", views.comment_create),  # 대댓글 작성
+    # path("create/", views.create, name="create"),
+    # path("<int:pk>/update/", views.update, name="update"),
+    # path("<int:pk>/delete/", views.delete, name="delete"),
+    # path("<int:post_pk>/comment/create", views.comment_create),  # 댓글 작성
+    # path(
+    #     "<int:post_pk>/comment/<int:comment_pk>/update", views.comment_update
+    # ),  # 댓글 수정
+    # path("<int:post_pk>/comment", views.comment),  # 댓글 보기
+    # path(
+    #     "<int:post_pk>/comment/<int:comment_pk>/delete", views.comment_delete
+    # ),  # 댓글 삭제
+    # path("<int:post_pk>/commemet/<int:parent_id>", views.comment_create),  # 대댓글 작성
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render, redirect
-from .models import Post
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from django.views.decorators.http import (
+    require_safe,
+    require_http_methods,
+    require_POST,
+)
 from django.http import JsonResponse
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
 def index(request):
-    posts = Post.objects.all()
-    context = {
-        "posts": posts,
-    }
-    return render(request, "posts/index.html", context)
+    if request.method == "GET":
+        print("들어옴")
+        posts = get_list_or_404(Post)
+        comments = get_list_or_404(Comment)
+        context = {
+            "posts": posts,
+            "comments": comments,
+        }
+        return render(request, "posts/index.html", context)
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            form.save()
+            return redirect("posts:index.html")
+        else:
+            form = PostForm()
+        context = {
+            "form": form,
+        }
+        return render(request, "posts/create.html", context)
 
 
 def detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    context = {"post": post}
+    post = get_object_or_404(Post, pk=pk)
+    comment_form = CommentForm()
+    comments = post.comment_set.all()
+    context = {
+        "post": post,
+        "comment_form": comment_form,
+        "comments": comments,
+    }
     return JsonResponse(context)
