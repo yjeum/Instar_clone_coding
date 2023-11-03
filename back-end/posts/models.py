c83764f07b2d0cc5162755435af840205ba8e223
YeJinEum <yjeum03@gmail.com>
2023-11-03 20:39:42 +0900

    [FIX] model manytomany field ondelete

--- back-end/posts/models.py
+++ back-end/posts/models.py
@@ -6,10 +6,9 @@ from django.conf import settings
 class Post(models.Model):
     uesr = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name='like_posts'
+        settings.AUTH_USER_MODEL, related_name="like_posts"
     )
-    image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    image = models.ImageField(upload_to="uploads/%Y/%m/%d/")
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -18,13 +17,12 @@ class Post(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='like_comments'
+        settings.AUTH_USER_MODEL, related_name="like_comments"
     )
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
-    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    parent_id = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-
